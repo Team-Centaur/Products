2ef6923fe14041d46b970f97e83db7c9927c58fb
Aaron Brandenberger <a.branden.mi@gmail.com>
2023-07-20 10:50:14 -0600

    IP changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const { Client } = require('pg')
 
 const connection = new Client({
   user: 'aaronbrandenberger',
-  host: '3.14.8.86',
+  host: '3.17.69.225',
   database: 'product_data',
   password: 'password',
   port: 5432,
